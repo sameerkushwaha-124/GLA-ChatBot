52f062d171975df5fbf88aa3d2c847c11cd5ca41
Sameer Kushwaha <sameerkushwaha2003@gmail.com>
2024-07-05 21:08:31 +0530

    Second Commit

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def user_input(user_question):
 
 def main():
     st.set_page_config("Chat PDF")
-    st.header("GLA ChatBot Help Support 💁")
+    st.header("GLA ChatBot Help & Support 💁")
 
     user_question = st.text_input("Ask a Question from the PDF Files")
 
